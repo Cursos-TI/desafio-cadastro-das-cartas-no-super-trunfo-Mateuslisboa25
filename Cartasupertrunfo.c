6dbd833d1bb1838453445d2a060fce1a83abd9b0
Mateuslisboa25 <mateuslisboa3030@gmail.com>
2025-05-06 23:00:19 +0000

    finalizado

--- Cartasupertrunfo.c
+++ Cartasupertrunfo.c
@@ -8,7 +8,7 @@ int main() {
     char nome[30];
     int populacao;
     float area;
-    double pib;
+    float pib;
     int pontosturisticos;
 
     printf("Qual o código da cidade:\n");
@@ -25,7 +25,7 @@ int main() {
     scanf("%f", &area);
 
     printf("Qual é o PIB da cidade?\n");
-    scanf("%lf", &pib); // %lf é mais apropriado para double
+    scanf("%f", &pib); // %lf é mais apropriado para double
 
     printf("Quantos pontos turísticos a cidade tem?\n");
     scanf("%d", &pontosturisticos);
@@ -35,9 +35,10 @@ int main() {
     printf("Código: %s\n", codigocidade);
     printf("Nome: %s", nome); // fgets já inclui a quebra de linha
     printf("População: %d habitantes\n", populacao);
-    printf("Área: %.2f km²\n", area);
-    printf("PIB: %.2e\n", pib);
+    printf("Área: %f km²\n", area);
+    printf("PIB: %.2f Bilhões de reais.\n", pib);
     printf("Pontos Turísticos: %d\n", pontosturisticos);
 
+    
     return 0;
 }
